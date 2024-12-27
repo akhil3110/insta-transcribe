2980fbee2d71da53e0cd0408938ae8b907351a41
Akhil <akhil1659@gmail.com>
2024-12-28 03:44:40 +0530

    small changes aws s3 client

--- lib/awsS3Client.ts
+++ lib/awsS3Client.ts
@@ -1,10 +1,14 @@
 import {S3Client} from "@aws-sdk/client-s3"
 
+if (!process.env.AWS_ACCESS_KEY_ID || !process.env.SECRET_ACCESS_KEY) {
+    throw new Error("AWS_ACCESS_KEY_ID and SECRET_ACCESS_KEY must be defined in environment variables.");
+}
+
 const s3  =  new S3Client({
     region: "ap-south-1",
     credentials:{
-        accessKeyId: "AKIAQUPS27TOJ35K7P4P",
-        secretAccessKey: "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"
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey:process.env.SECRET_ACCESS_KEY
     }
 })
 
