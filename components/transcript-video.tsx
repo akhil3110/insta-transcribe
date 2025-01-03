3703ea933e519f598ab72f7929ca268ece70f1f0
Akhil <akhil1659@gmail.com>
2025-01-04 01:24:21 +0530

    changes in transcript video loading component

--- components/transcript-video.tsx
+++ components/transcript-video.tsx
@@ -32,6 +32,7 @@ const TranscriptVideo = ({
     const [outlineColor,setOutlineColor] = useState('#000000')
     const [loaded, setLoaded] = useState(false);
     const [transcribing,setTranscribing] = useState(false)
+    const [progress, setProgress] = useState(1);
     
     const videoRef = useRef<any>(null)
     const ffmpegRef = useRef(new FFmpeg());
@@ -72,16 +73,16 @@ const TranscriptVideo = ({
           videoRef.current.onloadedmetadata = resolve;
         });
         const duration = videoRef.current.duration;
-        // ffmpeg.on('log', ({ message }) => {
-        //   const regexResult = /time=([0-9:.]+)/.exec(message);
-        //   if (regexResult && regexResult?.[1]) {
-        //     const howMuchIsDone = regexResult?.[1];
-        //     const [hours,minutes,seconds] = howMuchIsDone.split(':');
-        //     const doneTotalSeconds = hours * 3600 + minutes * 60 + seconds;
-        //     const videoProgress = doneTotalSeconds / duration;
-        //     setProgress(videoProgress);
-        //   }
-        // });
+        ffmpeg.on('log', ({ message }) => {
+          const regexResult = /time=([0-9:.]+)/.exec(message);
+          if (regexResult && regexResult?.[1]) {
+            const howMuchIsDone = regexResult?.[1];
+            const [hours,minutes,seconds] = howMuchIsDone.split(':');
+            const doneTotalSeconds = hours * 3600 + minutes * 60 + seconds;
+            const videoProgress = doneTotalSeconds / duration;
+            setProgress(videoProgress);
+          }
+        });
         await ffmpeg.exec([
           '-i', fileName,
           '-preset', 'ultrafast',
@@ -91,6 +92,7 @@ const TranscriptVideo = ({
         const data = await ffmpeg.readFile('output.mp4');
         videoRef.current.src = URL.createObjectURL(new Blob([data.buffer], {type: 'video/mp4'}));
         toast.error("adad")
+        setProgress(1)
       }
 
     
@@ -124,39 +126,42 @@ const TranscriptVideo = ({
                     </div>
                 </div>
             </div>
-                {
-                    transcribing ? (
-                        <div>
-
-                        </div>
-                    ) : (
-                        <div>
-                            
-                        </div>
-                    )
-                }
+            <div className="relative" style={{ width: "240px", height: "426px" }}>
                 <video
                     ref={videoRef}
                     controls
-                    style={{ width: "240px", height: "426px" }}
+                    style={{ width: "100%", height: "100%" }}
                     className="rounded-lg"
-                /> 
-                <Button onClick={transcode} className="font-bold text-lg" variant={'destructive'}>
-                    <Rocket />
-                    Apply Captions
-                </Button>
+                />
+                {progress && progress < 1 && (
+                    <div className="absolute inset-0 flex items-center justify-center bg-black/60 rounded-lg">
+                        <div className="flex flex-col gap-y-2 w-full items-center">
+                        <div className="w-full text-center">
+                            Loading ...
+                        </div>
+                        <div className="relative w-[80%]">
+                            <div className="h-4 bg-gray-200 rounded-full overflow-hidden">
+                                <div
+                                    className="h-full bg-gradient-to-r from-blue-500 to-blue-700"
+                                    style={{ width: `${progress * 100}%` }}
+                                />
+                            </div>
+                            <div className="absolute inset-0 flex items-center justify-center">
+                                <h3 className="text-red-700 text-sm font-bold">{Math.round(progress * 100)}%</h3>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                )}
             </div>
-              {/* <div className="h-[426px] w-[240px]">
-                <div role="status" className="h-full w-full flex justify-center items-center bg-slate-300/20 rounded-lg">
-                  <svg aria-hidden="true" className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
-                    <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
-                  </svg>
-                  <span className="sr-only">Loading...</span>
-                </div>
-              </div> */}
-            </div> 
-     );
+
+            <Button onClick={transcode} className="font-bold text-lg" variant={'destructive'}>
+                <Rocket />
+                Apply Captions
+            </Button>
+        </div>     
+    </div> 
+    );
 }
  
 export default TranscriptVideo;
