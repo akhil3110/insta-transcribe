3b54e786580fca64b8d3989afb3bbb0f91b49965
Akhil <akhil1659@gmail.com>
2025-01-07 20:25:10 +0530

    solved production isuues

--- auth.ts
+++ auth.ts
@@ -1,6 +1,5 @@
 import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
-import GitHub from "next-auth/providers/github"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import prisma from "./lib/db"
 
