d5d6d6ba55319e876b949d2e0e38b1fe3f73b5f0
Akhil <akhil1659@gmail.com>
2025-01-09 02:48:41 +0530

    added animation is home page

--- app/(homepage)/page.tsx
+++ app/(homepage)/page.tsx
@@ -1,23 +1,24 @@
-"use client"
-
+"use client";
 
 import HeroSection from "@/components/hero-section";
 import Pricing from "@/components/pricing";
 import Footer from "@/components/footer";
 import HowitWorks from "@/components/how-it-works";
 import UploadSection from "@/components/uploadSection";
-
-
-
+import { motion } from "framer-motion";
 
 const Home = () => {
-
   return (
     <section className="relative overflow-hidden py-16 sm:py-24">
-      <div className="max-w-7xl mx-auto flex flex-col  justify-center">
+      <motion.div
+        initial={{ scale: 0.8, opacity: 0 }}  // Start with smaller scale and invisible
+        animate={{ scale: 1, opacity: 1 }}   // End with normal scale and full opacity
+        transition={{ duration: 1, ease: "easeOut" }} // Slow transition with ease-out effect
+        className="max-w-7xl mx-auto flex flex-col justify-center"
+      >
         <HeroSection />
         <UploadSection />
-      </div>
+      </motion.div>
       <div>
         <HowitWorks />
       </div>
@@ -31,4 +32,4 @@ const Home = () => {
   );
 }
 
-export default Home
+export default Home;
