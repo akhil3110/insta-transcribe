597d783b2abf59415130e0afa054b19cc72bb532
Akhil <akhil1659@gmail.com>
2025-01-15 11:30:29 +0530

    added delete-video-modal and login-warning-modal

---
+++ components/modals/delete-video-modal.tsx
@@ -0,0 +1,69 @@
+import useModalStore from "@/store/modal-store";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "../ui/dialog";
+import { Button } from "../ui/button";
+import { deleteVideo } from "@/actions/deleteVideo";
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
+
+const DeleteVideoModal = ({ onVideoDeleted }: { onVideoDeleted?: (id: string) => void }) => {
+    const { isOpen, onClose, type, data } = useModalStore();
+    const isModalOpen = isOpen && type === "delete-video";
+    const router = useRouter();
+  
+    return (
+      <Dialog open={isModalOpen} onOpenChange={onClose}>
+        <DialogContent className="bg-neutral-800 text-white p-0 rounded-lg shadow-lg overflow-hidden">
+          <DialogHeader className="pt-8 px-6">
+            <DialogTitle className="text-2xl text-center font-bold text-white">
+              Delete Video
+            </DialogTitle>
+            <DialogDescription className="text-center text-neutral-400">
+              Are you sure you want to delete this video?
+            </DialogDescription>
+          </DialogHeader>
+          <DialogFooter className="bg-neutral-900 px-6 py-4">
+            <div className="flex items-center justify-between w-full">
+              <Button
+                onClick={() => 
+                    onClose()}
+                variant="ghost"
+                className="bg-neutral-700 text-white hover:bg-neutral-600"
+              >
+                Cancel
+              </Button>
+              <Button
+                onClick={async (e) => {
+                    e.preventDefault()
+                  const res = await deleteVideo(data?.videoId!);
+  
+                  if (res) {
+                    toast.success("Video Deleted Successfully");
+                    //@ts-ignore
+                    onVideoDeleted(data?.videoId!); // Update the state
+                  } else {
+                    toast.error("Something went wrong");
+                  }
+  
+                  router.refresh();
+                  onClose();
+                }}
+                className="bg-red-600 text-white hover:bg-red-500"
+              >
+                Confirm
+              </Button>
+            </div>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    );
+  };
+  
+export default DeleteVideoModal;
+  
