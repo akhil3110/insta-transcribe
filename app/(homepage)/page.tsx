e7b0b28c5a3b6f3a911d336201ef5bbb059898a2
Akhil <akhil1659@gmail.com>
2025-01-19 21:44:01 +0530

    demo button component added

--- app/(homepage)/page.tsx
+++ app/(homepage)/page.tsx
@@ -5,16 +5,17 @@ import Pricing from "@/components/pricing";
 import Footer from "@/components/footer";
 import HowitWorks from "@/components/how-it-works";
 import UploadSection from "@/components/uploadSection";
-import { motion } from "framer-motion"; 
+import { motion } from "framer-motion";
+import DemoButton from "@/components/demo-button";
+
 
 const Home = () => {
-
   return (
     <section className="relative overflow-hidden py-16 sm:py-24">
       <motion.div
-        initial={{ scale: 0.8, opacity: 0 }}  // Start with smaller scale and invisible
-        animate={{ scale: 1, opacity: 1 }}   // End with normal scale and full opacity
-        transition={{ duration: 1, ease: "easeOut" }} // Slow transition with ease-out effect
+        initial={{ scale: 0.8, opacity: 0 }}
+        animate={{ scale: 1, opacity: 1 }}
+        transition={{ duration: 1, ease: "easeOut" }}
         className="max-w-7xl mx-auto flex flex-col justify-center"
       >
         <HeroSection />
@@ -29,8 +30,9 @@ const Home = () => {
       <div>
         <Footer />
       </div>
+      <DemoButton /> {/* Add DemoButton here */}
     </section>
   );
-}
+};
 
 export default Home;
